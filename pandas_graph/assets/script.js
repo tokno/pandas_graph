8776da053a010efa45a0f17ba93d68854b7077d1
Takuya Okuno <iwashi1629@gmail.com>
2022-05-06 22:25:04 +0900

    初期表示時に図がめいっぱい表示されるようにした

--- pandas_graph/assets/script.js
+++ pandas_graph/assets/script.js
@@ -73,6 +73,14 @@ function renderPages(data) {
 }
 
 
+function changeGraphScale(scale) {
+    const graph = document.querySelector('svg')
+
+    graph.style.transform = `scale(${scale})`
+    graph.setAttribute('data-scale', scale)
+}
+
+
 function initializeGraph() {
     // nodeへのclass付与
     document.querySelectorAll('#graph-container svg .node').forEach(function(node) {
@@ -85,6 +93,19 @@ function initializeGraph() {
         const title = node.querySelector('title').textContent
         node.setAttribute('data-node-id', title)
     })
+
+    // 表示幅いっぱいに表示されるように調整
+    const container = document.querySelector('#graph-container')
+    const graph = document.querySelector('#graph-container svg')
+    const containerWidth = container.getBoundingClientRect().width
+    const containerHeight = container.getBoundingClientRect().height
+    const graphWidth = graph.getBoundingClientRect().width
+    const graphHeight = graph.getBoundingClientRect().height
+
+    changeGraphScale(Math.min(
+        containerWidth / graphWidth,
+        containerHeight / graphHeight,
+    ))
 }
 
 
@@ -100,25 +121,10 @@ function renderGraph(data) {
             initializeGraph()
         })
 
-    function changeGraphScale(event) {
-        const graph = document.querySelector('svg')
-        let scale = +graph.getAttribute('data-scale')
-    
-        const diff = event.deltaY * -0.002;
-        scale = scale * (1 + diff)
-    
-        // scale = Math.min(Math.max(0.05, scale), 1)
-    
-        graph.style.transform = `scale(${scale})`
-        graph.setAttribute('data-scale', scale)
-    
-        // TODO: マウスカーソルを中心に拡大、縮小させる
-    }
-
     // 拡大縮小とマウスでの移動
     let x = null
     let y = null
-        
+
     function moveGraphPosition(event) {
         const graph = document.querySelector('svg')
         const originalX = +(graph.style.left || '0px').replace('px', '')
@@ -136,8 +142,21 @@ function renderGraph(data) {
         x = event.pageX
         y = event.pageY
     }
-        
-    graphContainer.addEventListener('wheel', changeGraphScale)
+
+    graphContainer.addEventListener('wheel', function(event) {
+        const graph = document.querySelector('svg')
+        let scale = +graph.getAttribute('data-scale')
+    
+        const diff = event.deltaY * -0.002;
+        scale = scale * (1 + diff)
+    
+        // TODO: 拡大率に上限と下限を設ける
+    
+        changeGraphScale(scale)
+    
+        // TODO: マウスカーソルを中心に拡大、縮小させる
+    })
+
     graphContainer.onmousedown = function(event) {
         console.log(event)
         x = event.clientX
